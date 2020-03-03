b53bacebbe01d4fb593c4ab379b377aa9ebb6d33
D4YonSoundcloud <mattd4y@gmail.com>
2020-03-03 15:26:22 -0500

    3:26 recursion-2 3-3-20

--- recursion/recursion-2.js
+++ recursion/recursion-2.js
@@ -1,8 +1,14 @@
 function sumRange(num) {
-    if(num === 1) return 1;
-    return num + sumRange(num - 1);
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
 }
 
-sumRange(6)
+sumRange(3);
 
 //base case 1 // recursive call is num + sumRange(num - 1);
+
+// sumRange(3)
+//     return 3 + sumRange(2)
+//         return 2 + sumRange(1)
+//             return 1
+//                   3 + 2 + 1 = 6
