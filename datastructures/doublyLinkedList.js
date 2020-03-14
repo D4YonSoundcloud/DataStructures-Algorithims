39adfda4948e604dea5281aa881a7b588a2f3f56
d4yonsouncloude <mattd4y@gmail.com>
2020-03-13 19:48:41 -0600

    doublyLinkedList 9:48pm 3-13-20

--- datastructures/doublyLinkedList.js
+++ datastructures/doublyLinkedList.js
@@ -69,7 +69,7 @@ class doublyLinkedList {
 
   get(index) {
     if (index < -1 || index > this.length) return undefined;
-    if (index == 0) return this.hea ;
+    if (index == 0) return this.hea;
     if (index === this.length) return this.tail;
     if (index >= this.length / 2) {
       for (i = 0; i <= index; i++) {
@@ -84,4 +84,53 @@ class doublyLinkedList {
     }
     return current;
   }
+
+  set(val, index) {
+    if (index < -1 || index > this.length) return false;
+    if (index === 0) return !!(this.head.val = val);
+    if (index === this.length) return !!(this.tail.vail = val);
+    let node = this.get(index);
+    node ? (node.val = value) : false;
+    return true;
+  }
+
+  insert(val, index) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+    var newNode = new Node(val);
+    var old = this.get(index);
+    var prev = old.prev;
+    prev.next = newNode;
+    old.prev = newNode;
+    newNode.next = old;
+    newNode.prev = prev;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return !!this.shift();
+    if (index === this.length) return !!this.pop();
+    var foundNode = this.get(index);
+    var prev = foundNode.prev;
+    var after = foundNode.next;
+    prev.next = after;
+    after.prev = before;
+    foundNode.next = null;
+    foundNode.prev = null;
+    this.length--;
+    return foundNode;
+  }
 }
+
+// big O of doubly Linked List
+//insertion is O(1)
+//removal is O(1)
+//Searching - O(n) techincally O(n/2) but thats still O(n)
+//Access - O(n)
+
+//doublyLinkedList have extra pointers, makes things like working backwards much easier. things like browser history (every page has a next and a previous)
+//they can take up much more memory
+//better for finding nodes (can be done in half the time)
